907c411d5bd55ee1f9b7e4829df4d67d3f4ab004
Miguel Sanchez <miguelsanchez.inbox@gmail.com>
2023-03-22 14:58:11 -0400

    commit tree b79c5eba125913c8821ca3f38286a6cb2f6bf110

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Miguel',
+    streetAddress: '7 Pine Tree Rd',
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = {...obj};
+
+    newObj[key] = value;
+
+    return newObj; 
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+
+    obj[key] = value;
+
+    return obj; 
+}
+
+function deleteFromEmployeeByKey(obj, key, value){
+    const newObj = {...obj};
+
+    newObj[key] = value;
+
+    delete newObj.key; 
+
+    return newObj; 
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+
+    obj[key] = value;
+
+    delete obj.key;
+
+    return obj; 
+}
